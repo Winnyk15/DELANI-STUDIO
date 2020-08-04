7564cf670cab1d53b77a54ee46f6796fbce90f14
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-04 11:48:06 +0300

    add new changes

--- JS/index.js
+++ JS/index.js
@@ -69,7 +69,7 @@ $(document).ready(function(){
                alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
            }
            else if($("input#name").val() && $("input#email").val()){
-               alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
+               alert("Thank you for reaching out to us. If you wish to leave a message please type in on the message section..");
            }
           else{
             alert("Please provide correct name or email")
